c0be8512b3190ca85013c53dd4b7b799b651c543
firubott <runar.botten@finn.no>
2019-02-18 18:10:15 +0100

    Add latest solution

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <v-content>
       <h2>Demonstrates both the issue and the solution</h2>
       <BuggyDialog/>
+      <h2>I guess this is the right solution?</h2>
+      <PreferredDialog/>
       <h2>First attempt - This is just buggy...</h2>
       <ReallyBuggyDialog/>
     </v-content>
@@ -11,12 +13,14 @@
 
 <script>
 import BuggyDialog from './components/BuggyDialog'
+import PreferredDialog from './components/PreferredDialog'
 import ReallyBuggyDialog from './components/ReallyBuggyDialog'
 
 export default {
   name: 'App',
   components: {
       BuggyDialog,
+      PreferredDialog,
       ReallyBuggyDialog
   }
 }
---
+++ src/components/PreferredDialog.vue
@@ -0,0 +1,60 @@
+<template>
+    <ul>
+        <li v-for="item in items" :key="item.id">
+            {{ item.id }} - {{ item.text }}
+
+            <v-btn @click="prepareDelete(item.id)" icon color="red">
+                <v-icon>delete_forever</v-icon>
+            </v-btn>
+        </li>
+
+        <v-dialog persistent :value="dialogId">
+            <v-card>
+                <v-card-title class="headline">
+                    Delete item with ID {{dialogId}}?
+                </v-card-title>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn @click="dialogId = false" color="grey-lighten-2">No</v-btn>
+                    <v-btn @click="remove(dialogId)" color="green">Yes</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+    </ul>
+</template>
+
+<script>
+    /* eslint-disable */
+
+    export default {
+        methods: {
+            prepareDelete(id) {
+                this.dialogId = id;
+            },
+            remove(id) {
+                // Hold your horses! Can't trust this ID to be correct!!!!
+            }
+        },
+        data() {
+            return {
+                dialogId: false,
+                items: [
+                    {
+                        id: 1,
+                        text: 'Item 1'
+                    },
+                    {
+                        id: 2,
+                        text: 'Item 2'
+                    },
+                    {
+                        id: 3,
+                        text: 'Item 3'
+                    }
+                ]
+            }
+        }
+    }
+</script>
